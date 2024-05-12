81dfda94dbcc7991abe62a5fd9d51495013a76a1
bezhan2009 <karimovbezan0@gmail.com>
2024-05-12 17:52:55 +0300

    console app fix

--- connect_utils.py
+++ connect_utils.py
@@ -6,3 +6,17 @@ def check_conn(conn):
         return False
     else:
         return True
+
+"""
+def get_manually_connect():
+    db_name = request.form['db_name']
+    user = request.form['user']
+    password = request.form['password']
+    conn = manually_connect_p(db_name, user, password)
+    session['conn'] = conn
+
+    if conn is None:
+        return redirect_to_connect()
+    return redirect_to_index()
+    
+"""
