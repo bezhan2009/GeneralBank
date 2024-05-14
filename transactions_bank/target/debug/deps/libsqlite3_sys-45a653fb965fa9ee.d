975aa341ad7a4e17d92e04584ba14abc37a7590a
bezhan2009 <karimovbezan0@gmail.com>
2024-05-14 21:39:57 +0300

    fix rust

---
+++ transactions_bank/target/debug/deps/libsqlite3_sys-45a653fb965fa9ee.d
@@ -0,0 +1,11 @@
+C:\Users\Admin\PycharmProjects\again\transactions_bank\target\debug\deps\liblibsqlite3_sys-45a653fb965fa9ee.rmeta: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\error.rs C:\Users\Admin\PycharmProjects\again\transactions_bank\target\debug\build\libsqlite3-sys-5a3b066cec2c61e0\out/bindgen.rs
+
+C:\Users\Admin\PycharmProjects\again\transactions_bank\target\debug\deps\liblibsqlite3_sys-45a653fb965fa9ee.rlib: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\error.rs C:\Users\Admin\PycharmProjects\again\transactions_bank\target\debug\build\libsqlite3-sys-5a3b066cec2c61e0\out/bindgen.rs
+
+C:\Users\Admin\PycharmProjects\again\transactions_bank\target\debug\deps\libsqlite3_sys-45a653fb965fa9ee.d: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\error.rs C:\Users\Admin\PycharmProjects\again\transactions_bank\target\debug\build\libsqlite3-sys-5a3b066cec2c61e0\out/bindgen.rs
+
+C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\lib.rs:
+C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\libsqlite3-sys-0.23.2\src\error.rs:
+C:\Users\Admin\PycharmProjects\again\transactions_bank\target\debug\build\libsqlite3-sys-5a3b066cec2c61e0\out/bindgen.rs:
+
+# env-dep:OUT_DIR=C:\\Users\\Admin\\PycharmProjects\\again\\transactions_bank\\target\\debug\\build\\libsqlite3-sys-5a3b066cec2c61e0\\out
